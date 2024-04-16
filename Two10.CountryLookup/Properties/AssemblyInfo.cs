e8c903bbd39e0aa9b2228793b1af27cf24b5f99c
louie-sage <LouieSage@adas.com>
2024-04-16 16:10:40 +0100

    migrate to .net8

--- Two10.CountryLookup/Properties/AssemblyInfo.cs
+++ Two10.CountryLookup/Properties/AssemblyInfo.cs
@@ -1,16 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Two10.CountryLookup")]
-[assembly: AssemblyDescription("Lookup a country by lat/lng coordinates")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("two10degrees")]
-[assembly: AssemblyProduct("Two10.CountryLookup")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -21,16 +11,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("74f9747d-2b2b-4f0c-824d-bbbb9f0a0389")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
---
+++ Two10.CountryLookupUpdated.Tests/CountryLookUpIntegrationTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Two10.CountryLookup.Abstractions;
+using Two10.CountryLookup;
+using FluentAssertions;
+
+namespace Two10.CountryLookupUpdated.Tests
+{
+    public class CountryLookUpIntegrationTest
+    {
+        private readonly IReverseLookup _reverseLookup;
+
+        public CountryLookUpIntegrationTest()
+        {
+            _reverseLookup = new ReverseLookup();
+        }
+
+        [Fact]
+        public void TestDataLoad()
+        {
+            _reverseLookup.Regions.Length.Should().Be(394);
+
+            foreach (var country in _reverseLookup.Regions)
+            {
+                country.Name.Should().NotBeNullOrEmpty();
+                country.Code.Should().NotBeNullOrEmpty();
+            }
+
+            _reverseLookup.Regions.Should().Contain(x => x.Name == "United Kingdom");
+
+            var ukResult = _reverseLookup.Lookup(52, 0);
+            ukResult.Should().NotBeNull();
+            ukResult.Name.Should().Be("United Kingdom");
+        }
+    }
+}
